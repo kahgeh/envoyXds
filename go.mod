3982451b5f724ff17070020d3efb743fd47103f1
kahgeh.tan <kahgeh.tan@xero.com>
2019-04-24 22:09:01 +1000

    include basic sample code

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/kahgeh/envoyXds
 
 go 1.12
 
-require github.com/envoyproxy/go-control-plane v0.7.1
+require (
+	github.com/envoyproxy/go-control-plane v0.7.1
+	github.com/envoyproxy/protoc-gen-validate v0.0.14 // indirect
+	github.com/gogo/googleapis v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.4.1
+	google.golang.org/grpc v1.20.1
+)
--- main.go
+++ main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"net"
 
+	api "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
+	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	xds "github.com/envoyproxy/go-control-plane/pkg/server"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+type Hasher struct {
+}
+
+// ID function
+func (h Hasher) ID(node *core.Node) string {
+	if node == nil {
+		return "unknown"
+	}
+	return node.Id
+}
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 10000, "The server port")
+)
+
+func main() {
+	flag.Parse()
+	fmt.Println("Starting up...")
+	snapshotCache := cache.NewSnapshotCache(false, Hasher{}, nil)
+	server := xds.NewServer(snapshotCache, nil)
+	grpcServer := grpc.NewServer()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.WithError(err).Fatal("failed to listen")
+	}
+
+	discovery.RegisterAggregatedDiscoveryServiceServer(grpcServer, server)
+	api.RegisterEndpointDiscoveryServiceServer(grpcServer, server)
+	api.RegisterClusterDiscoveryServiceServer(grpcServer, server)
+	api.RegisterRouteDiscoveryServiceServer(grpcServer, server)
+	api.RegisterListenerDiscoveryServiceServer(grpcServer, server)
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.WithError(err).Fatal("failed to serve")
+		}
+	}()
+	fmt.Println("Started. Press enter to terminate...")
+	fmt.Scanln()
 }
